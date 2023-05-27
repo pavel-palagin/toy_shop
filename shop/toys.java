b930384aa4c4eb51aa0093a4448ca64af44201df
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2023-05-28 01:59:58 +0300

    Переопределение toString

--- shop/toys.java
+++ shop/toys.java
@@ -11,6 +11,16 @@ public class toys {
         this.count = count;
         this.id = id;
         this.frequency = frequency;
+
     }
+
+    @Override
+    public String toString() {
+
+        return "Toys{" +
+                "name=" + name +
+                ", count=" + count +
+                ", id=" + id +
+                '}';
 
 }
