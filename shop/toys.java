749aedb29f6daeecec998cac6d7ce17a0321b0ed
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2023-05-28 01:58:16 +0300

    конструктор toys

--- shop/toys.java
+++ shop/toys.java
@@ -1,5 +1,16 @@
 package shop;
 
 public class toys {
+    private String name;
+    private int count;
+    private int id;
+    private int frequency;
+
+    public toys(String name, int count, int id, int frequency) {
+        this.name = name;
+        this.count = count;
+        this.id = id;
+        this.frequency = frequency;
+    }
 
 }
